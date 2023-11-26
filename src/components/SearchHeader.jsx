01672f8684b67db96afe1930e4289461fff7604c
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2023-11-26 21:00:22 +0900

    feat : searchHeader

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
-import { Link ,useNavigate} from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link ,useNavigate, useParams} from 'react-router-dom'
+import {BsYoutube,BsSearch} from 'react-icons/bs'
 
 function SearchHeader() {
+  const {keyword}=useParams();
   const [text,setText]=useState('')
   const navigate = useNavigate()
 
@@ -15,13 +17,23 @@ function SearchHeader() {
     navigate(`/videos/${text}`)
   }
 
+  useEffect(()=>{
+    setText(keyword||"")
+  },[keyword])
+
   return (
-    <div>
-      <Link to="/">Youtube</Link>
-      <form onSubmit={handleSubmit}>
-        <input type='text' placeholder='search video' value={text} onChange={handleChange}/>
+    <header className='w-full flex p-4 text-2xl border-b border-zinc-600 mb-4'>
+      <Link to="/" className='flex items-center'>
+        <BsYoutube className='text-4xl text-brand'/>
+        <h1 className='font-bold ml-2 text-3xl'>Youtube</h1>
+      </Link>
+      <form className='w-full flex justify-center' onSubmit={handleSubmit}>
+        <input className='w-7/12 p-2 outline-none bg-black text-gray-50' type='text' placeholder='search video' value={text} onChange={handleChange}/>
+        <button className='bg-zinc-600 px-4'>
+          <BsSearch/>
+        </button>
       </form>
-    </div>
+    </header>
   )
 }
 
